8bd216a1b769621456de7bd0c6733963a7dbf8f6
digveshparab <digveshparab123@gmail.com>
2024-10-06 13:36:10 +0530

    about page mobile and desktop view - frontend

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,17 @@ const About = () => {
 
   return (
     <div className='about-main-container'>
-        about
+        <div className='about-content-holder'>
+          <h2>About Me</h2>
+          <span></span>
+          <p>
+            Frontend Developer with 1.5 years of experience building scalable web applications using HTML, CSS,
+            JavaScript, and React.js. Skilled in collaborating with cross-functional teams in fast-paced environments to
+            deliver high-quality solutions. Proficient with build tools like Webpack and version control using Git. Strong
+            problem-solving skills and a passion for continuous learning and improvement.
+          </p>
+
+        </div>
     </div>
   )
 }
