db0bf545265be2f54c9173fd96b2be0488d394a1
digveshparab <digveshparab123@gmail.com>
2024-10-06 13:35:37 +0530

    title page mobile and desktop view - frontend

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -4,7 +4,14 @@ const Title = () => {
 
   return (
     <div className='title-main-container'>
-        title
+      <div className='title-content-holder'>
+        <h1>Digvesh Parab</h1>
+        {/* <h3>SOFTWARE DEVELOPER</h3> */}
+        <div className='title-btn-line-holder'>
+          <button>LETS START !</button>
+          <div className='title-long-line'></div>
+        </div>
+      </div>
     </div>
   )
 }
