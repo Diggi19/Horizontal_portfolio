9b83c2aba4757451120dc80a4a0ec7f6ca99ecb6
digveshparab <digveshparab123@gmail.com>
2024-10-13 10:58:14 +0530

    contact page mobile and desktop view - frontend

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,16 @@ import './contact.css'
 const Contact = () => {
   return (
     <div className='contact-main-container'>
-        Contact
+        <div className='contact-main-holder'>
+          <h3>Let's Connect!</h3>
+          <p>I'm always eager to connect and explore new ideas. Whether it's a collaboration, a project, or an exciting opportunity, feel free to reach out!</p>
+          <button>
+            <a href="mailto:your-email@example.com">Get in Touch</a>
+          </button>        
+        </div>
+        <div className='contact-social-holder'>
+
+        </div>
     </div>
   )
 }
