8b311a99b61fa3a34fdac4bf1aaf351e0be9b2d4
digveshparab <digveshparab123@gmail.com>
2024-10-20 13:57:57 +0530

    contact and project page - a link color chage issue fixed - frontend

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className='contact-main-holder'>
           <h3>Let's Connect!</h3>
           <p>I'm always eager to connect and explore new ideas. Whether it's a collaboration, a project, or an exciting opportunity, feel free to reach out!</p>
-          <button>
+          <button className='contact-btn'>
             <a href="mailto:your-email@example.com">Get in Touch</a>
           </button>        
         </div>
